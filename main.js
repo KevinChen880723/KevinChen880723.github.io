14f409c5a95274dafcaa09ec925db28e20edfba4
KevinChen880723 <b10602120@mail.ntust.edu.tw>
2022-03-24 19:56:22 +0800

    Changed the background color of work description block to a dark color

--- main.js
+++ main.js
@@ -11,22 +11,26 @@ window.onload = function() {
         document.getElementById("ckmates-description").style.opacity = "1";
         document.getElementById("ckmates-description").style.padding = "20px";
         document.getElementById("ckmates-description").style.height = "100%";
+        document.getElementById("ckmates-description").style.border = "solid #aaa 2px";
     }
     function mouseOutCKmates() {
         document.getElementById("ckmates-description").style.opacity = "0";
         document.getElementById("ckmates-description").style.padding = "0px 20px";
         document.getElementById("ckmates-description").style.height = "0";
+        document.getElementById("ckmates-description").style.border = "solid #aaa 0px";
     }
 
     function mouseOverITRI() {
         document.getElementById("itri-description").style.opacity = "1";
         document.getElementById("itri-description").style.padding = "20px";
         document.getElementById("itri-description").style.height = "100%";
+        document.getElementById("itri-description").style.border = "solid #aaa 2px";
     }
     function mouseOutITRI() {
         document.getElementById("itri-description").style.opacity = "0";
         document.getElementById("itri-description").style.padding = "0px 20px";
         document.getElementById("itri-description").style.height = "0";
+        document.getElementById("itri-description").style.border = "solid #aaa 0px";
     }
     console.log("end");
 };
