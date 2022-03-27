eefe8d9f49926d5abe42b61fe520751caa699fd4
KevinChen880723 <b10602120@mail.ntust.edu.tw>
2022-03-27 23:57:32 +0800

    Almost finish without RWD

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
+let portfolio_title_array = [
+    "ITRI Depth Estimation Project",
+    "IoT Device Controller: Front-end Website",
+    "IoT Device Controller: IoT Device Simulator",
+    "Smart Life Washing Machine",
+    "HLS: NN Accelerator"
+];
+
+let portfolio_description_array = [
+    "In the self-driving car application, object detection and the distance to the objects are critical. We modified the architecture in \"Digging Into Self-Supervised Monocular Depth Estimation\" to a multiple decoder architecture. The project achieved using only the monocular camera to get the depth information in the meter unit and detect the objects simultaneously.",
+    "In the \"IoT Device Controller\" project, I developed a website as the GUI interface let the user control the device status. After the user presses the \"Confirm\" button, the system will deliver the instructions to control the device through AWS API Gateway, AWS Lambda, and AWS IoT services.",
+    "Raspberry Pi could be an IoT device that lets the user control the GPIOs by Python code. I didn't have the Raspberry Pi back then, so I wrote the Python code on the PC to simulate the device. As illustrated above, the program shows the new status when the user changes the device status.",
+    "333333333333333 33333333333333333333333333333333 3333333333333333333333333333333333333333333333333333 3333333333333333333333333333333333333 33333333333333333333333333333333",
+    "The deep learning model takes lots of time and power to execute. To reduce the energy and time, FPGA could help. HLS is a tool letting the software engineer easily build the customized FPGA device. In this project, I used HLS to build a neural network library consisting of common layers such as the fully connected, max-pooling, ReLU, and convolutional layer. I used my library to build an MNIST model to validate the correctness."
+];
+
+let portfolio_imgName_array = [
+    "./img/portfolio/depth-estimation.png",
+    "./img/portfolio/iot-front-end.png",
+    "./img/portfolio/iot-simulator.png",
+    "./img/portfolio/washingMachine.jpg",
+    "./img/portfolio/hls.png"
+]
+
+// let img_scale_ratio = [ 1.7560975609756098, 1, 2.16260162601626, 2.1105691056910567 ];
+let img_scale_ratio = [];
+
+console.log("hello");
+let img = new Image(), min_img_height = "99999", i = 0;
+for (i = 0 ; i < portfolio_imgName_array.length; i++) {
+    img.src = portfolio_imgName_array[i];
+    let normalized_height = img.height/img.width;
+    img_scale_ratio.push(normalized_height);
+    if (normalized_height < min_img_height) 
+        min_img_height = normalized_height;
+}
+for (i = 0; i < portfolio_imgName_array.length; i++) {
+    img_scale_ratio[i] /= min_img_height;
+}
+
+console.log(img_scale_ratio);
+
 window.onload = function() {
     let portfolio_idx = 0;
-    let portfolio_title_array = [
-        "ITRI Depth Estimation Project",
-        "IoT: Device Controller",
-        "Smart Life Washing Machine"
-    ];
-
-    let portfolio_description_array = [
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est illum quaerat quae pariatur quia, culpa recusandae molestiae aut minima voluptate.",
-        "22222222222222",
-        "333333333333333 33333333333333333333333333333333 3333333333333333333333333333333333333333333333333333 3333333333333333333333333333333333333 33333333333333333333333333333333"
-    ];
-
-    let portfolio_imgName_array = [
-        "./img/portfolio/depth-estimation.png",
-        "./img/portfolio/iot-front-end.png",
-        "./img/portfolio/washingMachine.jpg"
-    ]
-
     let ckmates = document.getElementById("ckmates-icon");
     let itri = document.getElementById("itri-icon");
     let view_toeic_cert = document.getElementById("view-toeic-cert");
@@ -38,6 +62,8 @@ window.onload = function() {
     // Initialize the contents in the portfolio section.
     portfolio_title.textContent = portfolio_title_array[portfolio_idx];
     portfolio_img.src = portfolio_imgName_array[portfolio_idx];
+    portfolio_img.style.width = 100 / img_scale_ratio[portfolio_idx] + "%";
+    portfolio_img.style.padding = "0% " + ((100 - (100 / img_scale_ratio[portfolio_idx])) / 2) + "%";
     portfolio_description.textContent = portfolio_description_array[portfolio_idx];
 
     // Set the contents of the functions.
@@ -79,6 +105,8 @@ window.onload = function() {
         else portfolio_idx = portfolio_title_array.length - 1;
         portfolio_title.textContent = portfolio_title_array[portfolio_idx];
         portfolio_img.src = portfolio_imgName_array[portfolio_idx];
+        portfolio_img.style.width = 100 / img_scale_ratio[portfolio_idx] + "%";
+        portfolio_img.style.padding = "0% " + ((100 - (100 / img_scale_ratio[portfolio_idx])) / 2) + "%";
         portfolio_description.textContent = portfolio_description_array[portfolio_idx];
     }
 
@@ -87,6 +115,8 @@ window.onload = function() {
         else portfolio_idx = 0;
         portfolio_title.textContent = portfolio_title_array[portfolio_idx];
         portfolio_img.src = portfolio_imgName_array[portfolio_idx];
+        portfolio_img.style.width = 100 / img_scale_ratio[portfolio_idx] + "%";
+        portfolio_img.style.padding = "0% " + ((100 - (100 / img_scale_ratio[portfolio_idx])) / 2) + "%";
         portfolio_description.textContent = portfolio_description_array[portfolio_idx];
     }
 
